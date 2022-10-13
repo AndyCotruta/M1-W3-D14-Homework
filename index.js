083d2391fa315efa3396ecddb4b8d4fa34f68630
AndyCotruta <acotruta18@gmail.com>
2022-10-13 21:04:06 +0200

    Almost there
    Need to add a way to not generate and not color the same number twice

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 window.onload = function () {
   createBingoNumbers();
   const inputNode = document.getElementById("clickToGenerate");
+  const clickToPlay = document.getElementById("generateRounds");
+  const clickToReset = document.getElementById("resetButton");
   inputNode.addEventListener("click", clickToGenerate);
+  clickToPlay.addEventListener("click", createBingoCards, true);
+  clickToPlay.addEventListener("click", resetInputValue, true);
+  clickToReset.addEventListener("click", resetGame);
 };
 
 const numberArray = [];
+const newNumberArray = [];
+const generatedNumberArray = [];
 
 const createBingoNumbers = function () {
   const bingoNode = document.getElementById("bingoBoard");
@@ -35,6 +42,8 @@ const clickToGenerate = function () {
   let random = randomNumber();
   inputNode.value = random;
   colorBingoNumber(random);
+  colorGameNumber(random);
+  generatedNumberArray.push(random);
 };
 
 // const colorBingoNumber = function (random) {
@@ -46,9 +55,59 @@ const clickToGenerate = function () {
 //   }
 // };
 
+const colorGameNumber = function (random) {
+  const bingoNumberNodes = document.getElementsByClassName("numberDesired");
+  for (let i = 0; i < bingoNumberNodes.length; i++) {
+    const numberNode = bingoNumberNodes[i];
+    if (bingoNumberNodes[i].innerText == random) {
+      numberNode.classList.add("redClass");
+    }
+  }
+};
+
 const colorBingoNumber = function (random) {
-  const bingoNumberNodes = document.getElementsByClassName("number");
-  const numberNode = bingoNumberNodes[random - 1];
-  console.log(numberNode);
+  const gameNumberNodes = document.getElementsByClassName("number");
+  const numberNode = gameNumberNodes[random - 1];
   numberNode.classList.add("redClass");
+};
+
+const createBingoCards = function () {
+  const inputNode = document.getElementById("desiredRounds");
+  const desiredRounds = inputNode.value;
+  const bingoCardNode = document.getElementById("bingoDesiredBoards");
+  if (desiredRounds == 0) {
+    alert("Please select a number and then click the 'Let's play' button");
+  } else {
+    for (let i = 0; i < desiredRounds; i++) {
+      for (let i = 0; i < 24; i++) {
+        const bingoNumberNode = document.createElement("div");
+        bingoNumberNode.className = "numberDesired";
+        if (i === 23) {
+          const breakLine = document.createElement("br");
+          breakLine.className = "breakLine";
+          bingoCardNode.appendChild(breakLine);
+        } else {
+          const h3 = document.createElement("h3");
+          h3.innerText = Math.ceil(Math.random() * 76);
+          newNumberArray.push(h3.innerText);
+          bingoNumberNode.appendChild(h3);
+          bingoCardNode.appendChild(bingoNumberNode);
+        }
+      }
+    }
+  }
+};
+
+const resetInputValue = function () {
+  const inputNode = document.getElementById("desiredRounds");
+  inputNode.value = 0;
+};
+
+const resetGame = function () {
+  const previousGame = document.getElementsByClassName("numberDesired");
+  console.log(previousGame);
+  for (let i = 0; i < previousGame.length; i++) {
+    const previousGameNumbers = previousGame[i];
+    previousGameNumbers.classList.add("resetGame");
+  }
 };
